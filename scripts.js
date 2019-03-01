883ceb3656ab02c80ec7ab9b2a7bbfdf27d09c2e
Charlotte Shih <shih.charlotte@gmail.com>
2019-02-28 20:30:14 -0800

    started API call to IGDB, made some minor CSS tweaks

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 'use strict';
 
+const igdbKey = '8b9e048aae57e52cf42765a9d130c173';
+
 function submitButtonHandler() {
     $('.game-form').on('submit', function() {
         event.preventDefault();
-        storeSearchTerm();
-        // console.log("Button works");
+        handleGameSearch();
+        console.log('Button works');
     });
 }
 
-function storeSearchTerm() {
-    let game = $('#game-search').val().trim();
-    console.log(game);
+function handleGameSearch() {
+    let gameTitle = $('#game-search').val().trim().toLowerCase();
+    console.log('Searched for: '+ gameTitle);
+    $('#game-search').val('');
+    fetchIgdb(gameTitle);
+    // FELIX: call your Twitch API function here?
+}
+
+function fetchIgdb(gameTitle) {
+    fetch(`https://api-v3.igdb.com/?user-key=${igdbKey}&fields=name,websites.url,genres.name,rating&search=${gameTitle}`)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
 }
 
 $(submitButtonHandler());

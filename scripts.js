a0198f25ef7a893830b3356f4f0c91f85ba3d9d1
Charlotte Shih <shih.charlotte@gmail.com>
2019-02-28 21:19:30 -0800

    still working on fetch request

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const igdbKey = '8b9e048aae57e52cf42765a9d130c173';
-
 function submitButtonHandler() {
     $('.game-form').on('submit', function() {
         event.preventDefault();
@@ -12,16 +10,25 @@ function submitButtonHandler() {
 
 function handleGameSearch() {
     let gameTitle = $('#game-search').val().trim().toLowerCase();
-    console.log('Searched for: '+ gameTitle);
+    console.log('Searched for: ' + gameTitle);
     $('#game-search').val('');
     fetchIgdb(gameTitle);
     // FELIX: call your Twitch API function here?
 }
 
 function fetchIgdb(gameTitle) {
-    fetch(`https://api-v3.igdb.com/?user-key=${igdbKey}&fields=name,websites.url,genres.name,rating&search=${gameTitle}`)
+    const options = {
+        headers: {
+            'user-key': '142f6aed468c4dbcd5e740fd58fc0b12',
+            accept: 'json'
+        },
+        mode: 'no-cors'
+    };
+
+    fetch(`https://api-v3.igdb.com/games/?search=bioshock&fields=name,websites.url,genres.name,rating`, options)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => console.log(responseJson))
+    .catch(e => { console.log("Error: ", e); });
 }
 
 $(submitButtonHandler());

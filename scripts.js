e43ff496c1bb5fa02f82b63790b22aa768113b9c
Charlotte Shih <shih.charlotte@gmail.com>
2019-03-02 12:59:41 -0800

    working with ajax

--- scripts.js
+++ scripts.js
@@ -17,38 +17,35 @@ function handleGameSearch() {
 function fetchGiantBomb(gameTitle) {
     const apiKey = `fec6b7750ced7ec24e9ff54a9b2aeea2b573d5a8`;
 
-    fetch(`http://www.giantbomb.com/api/search/?api_key=${apiKey}&format=jsonp&json_callback=jsonp&query=${gameTitle}`, {mode: 'no-cors'})
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
-    // .then(data => console.log(JSON.stringify(data)))
-    .catch(error => console.log(`ERROR: ${error}`));
+    // fetch(`http://www.giantbomb.com/api/search/?api_key=${apiKey}&format=jsonp&json_callback=displayResults&query=${gameTitle}`, {mode: 'no-cors'})
+    // // .then(response => response.text())
+    // .then(responseJson => displayResults(responseJson))
+    // .catch(error => console.log(`${error}`));
+    
 
-    // Looks like I need json_callback to = a callback function?
+    // need json_callback to = a callback function?
+    // getting an empty object in response to fetch request - that's why response.json() isn't working and it messes up everything else
 
 
 
 
     // AJAX IS NOT ALLOWED (MUST USE FETCH) BUT THIS WORKS
 
-    // $.ajax ({
-    //     type: 'GET',
-    //     dataType: 'jsonp',
-    //     crossDomain: true,
-    //     jsonp: 'json_callback',
-    //     url: `http://www.giantbomb.com/api/search/?format=jsonp&api_key=${apiKey}&query=${gameTitle}`,
-    //     // complete: function() {
-    //     //     console.log('done');
-    //     // },
-    //     success: function(response) {
-    //         console.log(response.results[0]);
-    //         $('.giantbomb-results').html(`
-    //         <h2>${response.results[0].name}</h2>
-    //         <img src="${response.results[0].image.thumb_url}" alt="${gameTitle} thumbnail">
-    //         <p>${response.results[0].deck}</p>
-    //         `);
-    //     }
-    // });
-
+    $.ajax ({
+        type: 'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'json_callback',
+        url: `http://www.giantbomb.com/api/search/?format=jsonp&api_key=${apiKey}&query=${gameTitle}`,
+        success: function(response) {
+            console.log('game info: ', response.results);
+            $('.giantbomb-results').html(`
+                <h2>${response.results[0].name}</h2>
+                <img src="${response.results[0].image.thumb_url}" alt="${gameTitle} thumbnail">
+                <p>${response.results[0].deck} <a href="${response.results[0].site_detail_url}">Read More...</a></p>
+            `);
+        }
+    });
 }
 
 $(submitButtonHandler);

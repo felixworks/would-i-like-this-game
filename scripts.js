54142d1123cfa5065a2158cdced76f413160edf3
Charlotte Shih <shih.charlotte@gmail.com>
2019-03-03 01:39:36 -0800

    minor styling tweaks

--- scripts.js
+++ scripts.js
@@ -7,8 +7,6 @@ function submitButtonHandler() {
         event.preventDefault();
         console.log("Button works");
         let userInput = $('input.game-search').val();
-        // console.log(userInput);
-        // $('input.game-search').val('');
         $('.twitch-clip-results').empty();
         generateTwitchRequest(userInput);
         handleGameSearch();
